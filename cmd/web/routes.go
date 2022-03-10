f5cdf4be07f0f07a758e8b7bb3efe60be9e1cabc
Talha2299 <ams@rms.com.au>
2022-03-10 15:45:22 +0500

    set static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,7 +19,11 @@ func Routes(app *config.AppConfig) http.Handler {
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Talha2299/bookings
 go 1.17
 
 require (
-	github.com/go-chi/chi/v5 v5.0.7 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/justinas/nosurf v1.1.1
 )
